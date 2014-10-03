ce3f7ad9dfdd8f2b35df82c216ea4f60f2a807e0
HKudrya <h.kudrya@hotmail.com>
2014-10-03 15:42:16 +0300

    изменения

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число элементов массива A1");
+            int size = int.Parse(Console.ReadLine());
+            int[] A1 = new int[size];
+            Arrs.CreateOneDimAr(A1);
+            Arrs.PrintAr1("A1", A1); 
         }
     }
 }
